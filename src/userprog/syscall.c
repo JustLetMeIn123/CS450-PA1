6d4289bb5484bb333924f77ecb368ef2560e7753
Justin Park <park3jw@dukes.jmu.edu>
2022-10-17 18:03:29 -0400

    syscall attempt

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -15,6 +15,87 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+
   printf ("system call!\n");
+  int call;
+  switch (call) {
+    case SYS_EXIT:
+       set_process_exitcode(thread_current()->tid, call);
+       struct process_info *pi = get_process_info(thread_current()->tid) ;
+       printf("%s: exit(%d)\n", pi->name , call);
+       thread_exit();
+      break;
+    case SYS_WAIT:
+       if (!check_ptr(f->esp+1)) {
+         exit(-1);
+         return;
+       }
+
+       tid_t pid = f->esp + 1;
+       f->eax = process_wait(pid);
+      break;
+    case SYS_READ:
+       if (!check_ptr(f->esp+1) || !check_ptr(f->esp+2) || !check_ptr(f->esp+3)) {
+        exit(-1);
+       return;
+       }
+
+       int fd = f->esp + 1;
+       void* buffer = f->esp + 2;
+       int len = f->esp + 3;
+
+       if (fd == 0) {
+         f->eax = len;
+       }
+       else {
+         //lock_acquire(&fil_lock);
+         struct file_info *fi = get_file_info(fd);
+         //if (!fi) {
+         //lock_release(&fil_lock);
+           f->eax = -1;
+         //}
+       //else {
+        int bytes_read = file_read(fi, buffer, len);
+        //lock_release(&fil_lock);
+        f->eax = bytes_read;
+       //}
+  }
+      break;
+    case SYS_WRITE:
+       if (!check_ptr(f->esp+1) || !check_ptr(f->esp+2) || !check_ptr(f->esp+3) ) {
+         exit(-1);
+       return;
+       }
+
+       int fd = f->esp + 1;
+       void* buffer = f->esp + 2;
+       unsigned int len = f->esp + 3;
+
+       if (fd == STDIN_FILENO) {
+         exit(-1);
+       return;
+       }
+       else if (fd == STDOUT_FILENO) {
+         putbuf(buffer, len);
+         f->eax = len;
+       }
+       else {
+       // lock_acquire(&fil_lock);
+        struct file_info *fi = get_file_info(fd);
+        if (fi == NULL) {
+         // lock_release(&fil_lock);
+          f->eax = -1;
+        }
+       else {
+        int bytes_written = file_write(fi, buffer, len);
+       // lock_release(&fil_lock);
+        f->eax = bytes_written;
+       }
+      }
+      break;
+    default:
+      break;
+  }
+    
   thread_exit ();
 }
