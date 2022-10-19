99b90501cde1e1a9a47898e48d599f9eff8fa130
Justin Park <park3jw@dukes.jmu.edu>
2022-10-19 18:06:31 -0400

    syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,6 +1,6 @@
 #include "userprog/syscall.h"
-#include <stdio.h>
 #include <syscall-nr.h>
+#include "lib/stdio.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
@@ -14,26 +14,6 @@ syscall_init (void)
 }
 
 
-struct process_info* get_process_info(tid_t pid) {
-  struct list_elem *e;
-
-  struct process_info* pi = NULL;
-  lock_acquire(&pil_lock);
-  for (e = list_begin (&process_info_list); e != list_end (&process_info_list);
-       e = list_next (e))
-  {
-      struct process_info *p = list_entry (e, struct process_info, elem);
-      if (p->pid == pid) {
-        pi = p;
-        break;
-      }
-  }
-
-  lock_release(&pil_lock);
-
-  return pi;
-}
-
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
@@ -44,7 +24,7 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_EXIT:
        set_process_exitcode(thread_current()->tid, call);
        struct process_info *pi = get_process_info(thread_current()->tid) ;
-       printf("%s: exit(%d)\n", pi->name , call);
+      // printf("%s: exit(%d)\n", pi.name, call);
        thread_exit();
       break;
     case SYS_WAIT:
@@ -89,27 +69,27 @@ syscall_handler (struct intr_frame *f UNUSED)
        return;
        }
 
-       int fd = f->esp + 1;
-       void* buffer = f->esp + 2;
-       unsigned int len = f->esp + 3;
+       int fds = f->esp + 1;
+       void* buffer2 = f->esp + 2;
+       int len2 = f->esp + 3;
 
-       if (fd == STDIN_FILENO) {
+       if (fds == STDIN_FILENO) {
          exit(-1);
        return;
        }
-       else if (fd == STDOUT_FILENO) {
-         putbuf(buffer, len);
-         f->eax = len;
+       else if (fds == STDOUT_FILENO) {
+         putbuf(buffer2, len2);
+         f->eax = len2;
        }
        else {
        // lock_acquire(&fil_lock);
-        struct file_info *fi = get_file_info(fd);
+        struct file_info *fi = get_file_info(fds);
         if (fi == NULL) {
          // lock_release(&fil_lock);
           f->eax = -1;
         }
        else {
-        int bytes_written = file_write(fi, buffer, len);
+        int bytes_written = file_write(fi, buffer2, len2);
        // lock_release(&fil_lock);
         f->eax = bytes_written;
        }
